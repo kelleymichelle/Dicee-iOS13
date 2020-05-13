17ab4a78fe339754a5781533040296b94c9c6ed5
Kelley Chaplain <kmchaplain@gmail.com>
2020-05-13 13:07:10 -0500

    adding IBOutlets to dice image views to connect to view controller

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        1
+        //used Image Literals to connect another image to code
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
